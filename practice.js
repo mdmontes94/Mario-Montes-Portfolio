eb33502da69bec27376700cea0648f342e0e06ae
Mario Montes <mdmontes94@gmail.com>
2024-01-22 11:39:02 -0600

    For loop added to practice.js

--- practice.js
+++ practice.js
@@ -16,3 +16,14 @@ var int = 75;
 int = int * 5;
 
 console.log("The int variable is equal to: " + int);
+
+let int2 = 7;
+for(let i = 1; i <= 5; i++)
+{
+    if (int2 % 2 != 0)
+    {
+        console.log(int2);
+    }
+    
+    int2 ++;
+}

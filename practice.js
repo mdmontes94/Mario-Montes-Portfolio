925936e69f35a168c3b81410dc7cd71c428dfc48
Mario Montes <mdmontes94@gmail.com>
2024-01-21 21:23:39 -0600

    Added a new page to the site, including .css and .js files

---
+++ practice.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+
+Name: Mario Montes
+Date: 01/21/24
+
+*/
+
+var noise = prompt("What is your favorite 'background noise' TV show or movie?");
+
+console.log("The noise variable is equal to: \n " + noise);
+
+var int = 75;
+
+int = int * 5;
+
+console.log("The int variable is equal to: " + int);

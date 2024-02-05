36ee079b45eb6768420cc1be4774d4eaab1ccd15
Mario Montes <mdmontes94@gmail.com>
2024-02-04 22:59:47 -0600

    Added assignment requirements

--- dom.js
+++ dom.js
@@ -1,2 +1,27 @@
 "use strict";
 
+var containerDiv = document.querySelector("#container");
+
+const addText = () => 
+{
+    for (let i = 1; i <= 5; i++)
+    {
+        var newParagraph = document.createElement("p");
+        newParagraph.textContent = "Family";
+
+        containerDiv.appendChild(newParagraph);
+    }
+};
+
+const changeBackground = () => 
+{
+    document.body.style.backgroundColor = "red";
+
+    containerDiv.style.backgroundColor = "blue";
+
+    containerDiv.style.color = "white";
+};
+
+document.querySelector("#add").addEventListener("click", addText);
+
+document.querySelector("#change").addEventListener("click", changeBackground);

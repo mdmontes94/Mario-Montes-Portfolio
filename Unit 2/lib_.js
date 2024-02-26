80624a983ca0b45db900fccb20d079f944af8e34
Mario Montes <mdmontes94@gmail.com>
2024-02-26 00:53:07 -0600

    Added code

--- Unit 2/lib_.js
+++ Unit 2/lib_.js
@@ -1,2 +1,26 @@
 "use strict";
 
+const select = selector => document.querySelector(selector);
+
+class Person
+{
+    constructor(fname, lname)
+    {
+        this.firstName = fname;
+        this.lastName = lname;
+    }
+    
+    get fullName()
+    {
+        return this.firstName + " " + this.lastName;
+
+    }
+
+    logValues()
+    {
+        console.log(this.firstName);
+        console.log(this.lastName);
+        console.log(this.fullName);
+    }
+
+}
--- Unit 2/objects.js
+++ Unit 2/objects.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const person1 = new Person ("Grace", "Hopper");
+
+const person2 = new Person ("Ada", "Lovelace");
+
+person1.logValues();    

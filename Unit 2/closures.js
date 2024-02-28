0390fb4cfbc974e7b4ab05bea8690e81c492fa52
Mario Montes <mdmontes94@gmail.com>
2024-02-28 11:36:08 -0600

    Added closure

--- Unit 2/closures.js
+++ Unit 2/closures.js
@@ -1,3 +1,50 @@
 "use strict";
 
 var movesDiv = document.querySelector(".moves");
+
+function createPlayer(name, hp, ...moves)
+{
+    var name = name;
+    var hp = hp;
+    var moves = moves;
+
+    return {
+       getName()
+       {
+        return(name);
+       },
+
+       getHP()
+       {
+        return(hp);
+       },
+
+       appendMoves()
+       {
+        
+        for(let move of moves)
+        {
+            var newParagraph = document.createElement("p");
+            newParagraph.textContent = move;
+    
+            movesDiv.appendChild(newParagraph);
+        }
+
+       }
+    };
+}
+
+var vanguardSpectre = createPlayer("Shepard", 500, "Biotic Charge", "Nova", "Incendiary Ammo");
+
+vanguardSpectre.appendMoves();
+console.log(vanguardSpectre.getName(), vanguardSpectre.getHP());
+
+var wildWitch = createPlayer("Morrigan", 194, "Winter's Grasp", "Mind Blast", "Drain Life")
+
+wildWitch.appendMoves();
+console.log(wildWitch.getName(), wildWitch.getHP());
+
+var consularScoundrel = createPlayer("Bindo", 80, "Rapid Shot", "Stasis Field", "Force Heal");
+
+consularScoundrel.appendMoves();
+console.log(consularScoundrel.getName(), consularScoundrel.getHP());

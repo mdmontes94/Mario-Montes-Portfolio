c72b51cfdc1a16a395708497791b940b15f9b672
Mario Montes <mdmontes94@gmail.com>
2024-03-13 21:44:11 -0500

    Finished code for async.js

--- Unit 2/async.js
+++ Unit 2/async.js
@@ -1,2 +1,39 @@
 "use strict";
 
+async function createRace()
+{
+    var runner1 = false;
+    var runner2 = false;
+
+    var loser = "";
+
+    var runner1Go = new Promise((resolve) => 
+    {
+        setTimeout (() => {
+            loser = "runner1";
+            resolve(true);
+
+        },  3000);
+    });
+
+    var runner2Go = new Promise((resolve) => 
+    {
+        setTimeout (() => {
+            loser = "runner2"
+            resolve(true);
+
+        }, 1000);
+    });
+
+    runner1 = await runner1Go;
+    runner2 = await runner2Go;
+
+    var results = [runner1, runner2, loser]
+
+    return(results);
+}
+
+createRace().then((results) =>
+{
+    console.log("Loser: ", results[2]);
+});

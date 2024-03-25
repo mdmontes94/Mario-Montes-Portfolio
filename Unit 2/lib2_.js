092e606fb5c3c31c8b95041992a8b87b1794da0c
Mario Montes <mdmontes94@gmail.com>
2024-03-24 21:09:41 -0500

    Finished Unit 2 Project

--- Unit 2/lib2_.js
+++ Unit 2/lib2_.js
@@ -1,1 +1,120 @@
 "use strict";
+
+class Warrior
+{
+    constructor(type)
+    {
+        this.type = type;
+        this.hp = 0;
+        this.initialMoveset = "";
+    }
+
+    buildWarrior()
+    {
+        if (this.type == "Barbarian")
+        {
+            this.hp = 12;
+            this.initialMoveset = "Rage, Danger Sense, Reckless Attack";
+            
+            var profilePicture = document.createElement("img");
+            profilePicture.src = "barbarian.png"
+            movesDiv.appendChild(profilePicture);
+            
+            var warriorClass = document.createElement("p");
+            warriorClass.textContent = "Barbarian";
+            movesDiv.appendChild(warriorClass);
+            
+            var hp = document.createElement("p");
+            hp.textContent = "HP: " + this.hp;
+            movesDiv.appendChild(hp)
+
+            var initialMoveset = document.createElement("p");
+            initialMoveset.textContent ="Initial Moveset: " + this.initialMoveset;
+            movesDiv.appendChild(initialMoveset);
+        }
+        else if(this.type == "Bard")
+        {
+            this.hp = 8;
+            this.initialMoveset = "Charm Person, Cure Wounds, Comprehend Languages";
+            
+            var profilePicture = document.createElement("img");
+            profilePicture.src = "bard.png"
+            movesDiv.appendChild(profilePicture);
+            
+            var warriorClass = document.createElement("p");
+            warriorClass.textContent = "Bard";
+            movesDiv.appendChild(warriorClass);
+            
+            var hp = document.createElement("p");
+            hp.textContent = "HP: " + this.hp;
+            movesDiv.appendChild(hp);
+
+            var initialMoveset = document.createElement("p");
+            initialMoveset.textContent = "Initial Moveset: " + this.initialMoveset;
+            movesDiv.appendChild(initialMoveset);
+        }
+        else if(this.type == "Paladin")
+        {
+            this.hp = 10;
+            this.initialMoveset = "Bless, Detect Evil and Good, Divine Favor";
+            
+            var profilePicture = document.createElement("img");
+            profilePicture.src = "paladin.png"
+            movesDiv.appendChild(profilePicture);
+            
+            var warriorClass = document.createElement("p");
+            warriorClass.textContent = "Paladin";
+            movesDiv.appendChild(warriorClass);
+            
+            var hp = document.createElement("p");
+            hp.textContent = "HP: " + this.hp;
+            movesDiv.appendChild(hp);
+
+            var initialMoveset = document.createElement("p");
+            initialMoveset.textContent = "Initial Moveset: " + this.initialMoveset;
+            movesDiv.appendChild(initialMoveset);
+        }
+        else if(this.type == "Ranger")
+        {
+            this.hp = 10;
+            this.initialMoveset = "Hunter's Mark, Animal Friendship, Fog Cloud";
+            
+            var profilePicture = document.createElement("img");
+            profilePicture.src = "ranger.png"
+            movesDiv.appendChild(profilePicture);
+            
+            var warriorClass = document.createElement("p");
+            warriorClass.textContent = "Ranger";
+            movesDiv.appendChild(warriorClass);
+            
+            var hp = document.createElement("p");
+            hp.textContent = "HP: " + this.hp;
+            movesDiv.appendChild(hp);
+
+            var initialMoveset = document.createElement("p");
+            initialMoveset.textContent = "Initial Moveset: " + this.initialMoveset;
+            movesDiv.appendChild(initialMoveset);
+        }
+        else if(this.type == "Warlock")
+        {
+            this.hp = 8;
+            this.initialMoveset = "Hellish Rebuke, Mage Armor, Magic Missile";
+            
+            var profilePicture = document.createElement("img");
+            profilePicture.src = "warlock.png"
+            movesDiv.appendChild(profilePicture);
+            
+            var warriorClass = document.createElement("p");
+            warriorClass.textContent = "Warlock";
+            movesDiv.appendChild(warriorClass);
+            
+            var hp = document.createElement("p");
+            hp.textContent = "HP: " + this.hp;
+            movesDiv.appendChild(hp);
+
+            var initialMoveset = document.createElement("p");
+            initialMoveset.textContent = "Initial Moveset: " + this.initialMoveset;
+            movesDiv.appendChild(initialMoveset);
+        }       
+    }
+}
--- Unit 2/project.js
+++ Unit 2/project.js
@@ -1,2 +1,25 @@
 "use strict";
+const select = selector => document.querySelector(selector);
 
+var movesDiv = document.querySelector(".moves");
+
+function getWarriorType()
+{
+    var warriorType = select("#type").value;
+
+    return warriorType;
+}
+
+const createWarrior = () =>
+{
+    var newWarrior = new Warrior(getWarriorType());
+
+    console.log(newWarrior);
+
+    setTimeout (() => {
+        newWarrior.buildWarrior();
+
+    }, 2500);
+}
+
+select("#submit").addEventListener("click", createWarrior);

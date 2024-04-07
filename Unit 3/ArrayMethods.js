ee3ae9937aca4075f838abdfbcbe228084595e03
Mario Montes <mdmontes94@gmail.com>
2024-04-06 20:08:25 -0500

    Coded the Array Methods files and added a navicon.

--- Unit 3/ArrayMethods.js
+++ Unit 3/ArrayMethods.js
@@ -1,2 +1,61 @@
 "use strict";
 
+const select = selector => document.querySelector(selector);
+
+let values = [];
+
+function update()
+{
+    var outputParagraph = select("#output");
+
+    outputParagraph.innerHTML = values.reduce((total, value, index) =>{
+        return total + "<br>" + (index + 1) + ". " + value;
+    }, '');
+
+    select("#input").value = "";
+}
+
+function pushToArray()
+{
+    var input = select("#input").value;
+    
+    values.push(input);
+    update();
+}
+
+function popFromArray()
+{
+    values.pop();
+
+    update();
+}
+
+function unshiftToArray()
+{
+    var input = select("#input").value;
+    
+    values.unshift(input);
+    update();
+}
+
+function shiftFromArray()
+{
+    values.shift();
+
+    update();
+}
+
+function arrMap()
+{
+    var input = select("#input").value;
+
+    values = values.map(value  => input + value);
+
+    update();
+}
+
+select("#push").addEventListener("click", pushToArray);
+select("#pop").addEventListener("click", popFromArray);
+select("#unshift").addEventListener("click", unshiftToArray);
+select("#shift").addEventListener("click", shiftFromArray);
+select("#map").addEventListener("click", arrMap);

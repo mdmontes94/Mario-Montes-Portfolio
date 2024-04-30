8d6b93e68b6e70e18de7dd3b8f441877cb492c17
Mario Montes <mdmontes94@gmail.com>
2024-04-29 22:00:23 -0500

    Added .png files and finished code for the .html, .css and .js files

--- Final Project/lib_.js
+++ Final Project/lib_.js
@@ -16,9 +16,9 @@ class Kid
             picture.src = "bluey.png"
             document.querySelector(this.cell).appendChild(picture);
             
-            /*var warriorClass = document.createElement("p");
-            warriorClass.textContent = "Barbarian";
-            movesDiv.appendChild(warriorClass);*/
+            var nameplate = document.createElement("p");
+            nameplate.textContent = "Bluey";
+            document.querySelector(this.cell).appendChild(nameplate);
         }
         else if(this.name == "Winton")
         {
@@ -26,39 +26,39 @@ class Kid
             picture.src = "winton.png"
             document.querySelector(this.cell).appendChild(picture);
             
-            /*var warriorClass = document.createElement("p");
-            warriorClass.textContent = "Bard";
-            movesDiv.appendChild(warriorClass);*/
+            var nameplate = document.createElement("p");
+            nameplate.textContent = "Winton";
+            document.querySelector(this.cell).appendChild(nameplate);
         }
         else if(this.name == "Indy")
         {
             var picture = document.createElement("img");
             picture.src = "indy.png"
             document.querySelector(this.cell).appendChild(picture);
-            
-            /*var warriorClass = document.createElement("p");
-            warriorClass.textContent = "Paladin";
-            movesDiv.appendChild(warriorClass);*/
+
+            var nameplate = document.createElement("p");
+            nameplate.textContent = "Indy";
+            document.querySelector(this.cell).appendChild(nameplate);
         }
         else if(this.name == "Bingo")
         {
             var picture = document.createElement("img");
             picture.src = "bingo.png"
             document.querySelector(this.cell).appendChild(picture);
-            
-            /*var warriorClass = document.createElement("p");
-            warriorClass.textContent = "Ranger";
-            movesDiv.appendChild(warriorClass);*/
+
+            var nameplate = document.createElement("p");
+            nameplate.textContent = "Bingo";
+            document.querySelector(this.cell).appendChild(nameplate);
         }
         else if(this.name == "Snickers")
         { 
             var picture = document.createElement("img");
             picture.src = "snickers.png"
             document.querySelector(this.cell).appendChild(picture);
-            
-            /*var warriorClass = document.createElement("p");
-            warriorClass.textContent = "Warlock";
-            movesDiv.appendChild(warriorClass);*/
+
+            var nameplate = document.createElement("p");
+            nameplate.textContent = "Snickers";
+            document.querySelector(this.cell).appendChild(nameplate);
         }       
     }
 }
--- Final Project/project.js
+++ Final Project/project.js
@@ -2,6 +2,46 @@
 
 const select = selector => document.querySelector(selector);
 
+class GraphNode {
+    constructor(data)
+    {
+        this.data = data;
+        this.edges = [];
+    }
+
+    connect(node)
+    {
+        this.edges.push(node)
+        node.edges.push(this);
+    }
+}
+
+var graph = [];
+
+graph.push(new GraphNode(select("#cell1")));
+graph.push(new GraphNode(select("#cell2")));
+graph.push(new GraphNode(select("#cell3")));
+graph.push(new GraphNode(select("#cell4")));
+graph.push(new GraphNode(select("#cell5")));
+graph.push(new GraphNode(select("#cell6")));
+graph.push(new GraphNode(select("#cell7")));
+graph.push(new GraphNode(select("#cell8")));
+graph.push(new GraphNode(select("#cell9")));
+
+graph[0].connect(graph[1]);
+graph[1].connect(graph[2]);
+graph[3].connect(graph[4]);
+graph[4].connect(graph[5]);
+graph[6].connect(graph[7]);
+graph[7].connect(graph[8]);
+
+graph[0].connect(graph[3]);
+graph[1].connect(graph[4]);
+graph[2].connect(graph[5]);
+graph[3].connect(graph[6]);
+graph[4].connect(graph[7]);
+graph[5].connect(graph[8]);
+
 function chooseKid()
 {
     var choice = select("#kid").value;
@@ -24,13 +64,57 @@ const initialPosition = () =>
     
 };
 
+const remainingPlayers = () =>
+{
+    var cellsWithPlayers = graph.filter(GraphNode => GraphNode.data.textContent.trim() != "");
+
+    if(cellsWithPlayers.length == 1)
+    {
+        var winner = cellsWithPlayers[0].data.textContent.trim();
+        window.alert(winner + " is the winner!");
+
+        cellsWithPlayers[0].data.style.backgroundColor = "green";
+    }
+};
+
+var removedCells = [];
+
 const randomCell = () =>
 {
     setTimeout (() => {
-        var num = Math.floor(Math.random() * 9);
-        var selection = document.getElementById("cell" + num);
-        selection.style.backgroundColor = "red"; 
-    }, 2000);
+        var num; 
+        do
+        {
+           num = Math.floor(Math.random() * 9);
+        }
+        while(removedCells.includes(num));
+
+        var selection = document.getElementById("cell" + (num + 1));
+        selection.style.backgroundColor = "red";
+
+        var name = selection.textContent.trim();
+
+        if(name != "")
+        {
+            window.alert("Oh no! " + name + " is out!");
+            selection.textContent = "";
+            graph.splice(num, 1);
+            removedCells.push(num);
+
+            for (let GraphNode of graph)
+            {
+                GraphNode.edges = GraphNode.edges.filter(edge => edge != graph[num]);
+
+                GraphNode.edges = GraphNode.edges.filter(edge => !removedCells.includes(graph.indexOf(edge)));
+            }
+        }
+        else
+        {
+            window.alert("Every one is safe! For now...");
+        }
+
+        remainingPlayers();
+    }, 1000);
 };
 
 select("#submit").addEventListener("click", initialPosition);
